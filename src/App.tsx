896ebf2db4b3b403c9bb03d9c0ba8e111c687290
yujin113 <claire9585@sookmyung.ac.kr>
2022-11-03 17:10:18 +0900

    [feat] app.tsx 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import LoginPage from "./components/LoginPage/LoginPage";
@@ -6,19 +6,29 @@ import SignUpPage from "./components/LoginPage/SignUpPage";
 import ChatPage from "./components/ChatPage/ChatPage";
 import ChatroomPage from "./components/ChatPage/ChatroomPage";
 import Menubar from "./components/Menubar";
+import AddRoomPage from "./components/ChatPage/AddRoomPage";
+import EnterRoomModal from "./components/ChatPage/EnterRoomModal";
+import AboutPage from "./components/AboutPage";
+
+export const UrlContext = createContext("");
 
 function App() {
   return (
-    <BrowserRouter>
-      <Menubar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signUp" element={<SignUpPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/chatroom" element={<ChatroomPage />} />
-      </Routes>
-    </BrowserRouter>
+    <UrlContext.Provider value={"http://localhost:8080"}>
+      <BrowserRouter>
+        <Menubar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
+          <Route path="/chat/:roomId" element={<ChatPage />} />
+          <Route path="/chatroom" element={<ChatroomPage />} />
+          <Route path="/addroom" element={<AddRoomPage />} />
+          <Route path="/enterRoom" element={<EnterRoomModal />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UrlContext.Provider>
   );
 }
 
