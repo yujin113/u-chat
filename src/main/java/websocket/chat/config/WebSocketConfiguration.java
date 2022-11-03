1cdece5753469ee69b8fb8a89cb562fe16b35d10
yujin113 <claire9585@sookmyung.ac.kr>
2022-11-03 17:48:47 +0900

    [feat] config 수정
    - 리액트 cors 이슈로 인한 webconfig 추가
    - websocketconfig 수정

--- src/main/java/websocket/chat/config/WebSocketConfiguration.java
+++ src/main/java/websocket/chat/config/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package websocket.chat.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
@@ -11,15 +12,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
+    @Autowired
+    WebSocketHandler webSocketHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalingSocketHandler(), "/chat/{roomId}").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/chat/{roomId}").setAllowedOrigins("*");
     }
-
-    @Bean
-    public WebSocketHandler signalingSocketHandler() {
-        return new websocket.chat.service.WebSocketHandler();
-    }
-
-
 }
