0d62ba346844e312ceeff3bd040eb6cc1418ac55
yujin113 <claire9585@sookmyung.ac.kr>
2022-11-03 17:41:19 +0900

    [feat] 채팅 화면 완성, api 연결

--- src/components/ChatPage/ChatPage.tsx
+++ src/components/ChatPage/ChatPage.tsx
@@ -1,8 +1,76 @@
 import React from "react";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+import { UrlContext } from "../../App";
+
+interface RoomInfo {
+  id: number;
+  name: string;
+}
+
+type Message = {
+  messageId: number;
+  username: string;
+  sender: boolean;
+  content: string;
+  createdAt: string;
+};
 
 export default function ChatPage() {
+  const [count, setCount] = useState(0);
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const userId = localStorage.getItem("userId");
+  const username = localStorage.getItem("username");
+
+  const baseUrl = useContext(UrlContext);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const room = location.state as RoomInfo;
+
+  const getMessageInfo = () => {
+    axios
+      .get(baseUrl + `/api/chatroom/chats/${room.id}/${userId}`)
+      .then((response) => {
+        setMessages(response.data.data.messages);
+      });
+  };
+  const getRoomInfo = () => {
+    axios.get(baseUrl + `/api/chatroom/info/${room.id}`).then((response) => {
+      setCount(response.data.data.count);
+      const people: string[] = [];
+      response.data.data.users.map((user: string, index: number) => {
+        people.push(user);
+      });
+      setParticipants(people);
+    });
+  };
+
+  const leaveRoom = () => {
+    axios
+      .get(baseUrl + `/api/chatroom/leave/${room.id}/${userId}`)
+      .then((response) => {
+        var rooms = JSON.parse(localStorage.getItem("rooms") || "[]");
+        rooms.splice(rooms.indexOf(room.id), 1);
+        localStorage.setItem("rooms", JSON.stringify(rooms));
+        navigate("/")
+      });
+  };
+
+  useEffect(() => {
+    getRoomInfo();
+    getMessageInfo();
+    // window.location.reload();
+  }, []);
+
+  // useEffect(getRoomInfo, []);
+  // useEffect(getMessageInfo, []);
+
   return (
-    <div className="flex h-screen antialiased text-gray-800">
+    <div className="flex px-4 h-screen antialiased text-gray-800">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
         <div className="flex flex-col py-8 pl-6 pr-2 w-64 bg-white flex-shrink-0">
           <div className="flex flex-row items-center justify-center h-12 w-full">
@@ -22,9 +90,9 @@ export default function ChatPage() {
                 ></path>
               </svg>
             </div>
-            <div className="ml-2 font-bold text-2xl">QuickChat</div>
+            <div className="ml-2 font-bold text-2xl">{room.name}</div>
           </div>
-          <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
+          {/* <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
             <div className="h-20 w-20 rounded-full border overflow-hidden">
               <img
                 src="https://avatars3.githubusercontent.com/u/2763884?s=128"
@@ -40,63 +108,48 @@ export default function ChatPage() {
               </div>
               <div className="leading-none ml-1 text-xs">Active</div>
             </div>
-          </div>
+          </div> */}
           <div className="flex flex-col mt-8">
-            <div className="flex flex-row items-center justify-between text-xs">
-              <span className="font-bold">Active Conversations</span>
-              <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
-                4
+            <div className="flex flex-row items-center justify-between text-s">
+              <span className="font-bold">참가자</span>
+              <span className="flex items-center justify-center bg-gray-300 h-5 w-5 rounded-full">
+                {count}
               </span>
             </div>
-            <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
-              <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-                <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
-                  H
-                </div>
-                <div className="ml-2 text-sm font-semibold">Henry Boyd</div>
-              </button>
-              <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-                <div className="flex items-center justify-center h-8 w-8 bg-gray-200 rounded-full">
-                  M
-                </div>
-                <div className="ml-2 text-sm font-semibold">Marta Curtis</div>
-                <div className="flex items-center justify-center ml-auto text-xs text-white bg-red-500 h-4 w-4 rounded leading-none">
-                  2
-                </div>
-              </button>
-              <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-                <div className="flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full">
-                  P
-                </div>
-                <div className="ml-2 text-sm font-semibold">Philip Tucker</div>
-              </button>
-              <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-                <div className="flex items-center justify-center h-8 w-8 bg-pink-200 rounded-full">
-                  C
-                </div>
-                <div className="ml-2 text-sm font-semibold">Christine Reid</div>
-              </button>
-              <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-                <div className="flex items-center justify-center h-8 w-8 bg-purple-200 rounded-full">
-                  J
-                </div>
-                <div className="ml-2 text-sm font-semibold">Jerry Guzman</div>
-              </button>
+            <div className="flex flex-col space-y-1 mt-4 -mx-2 h-100 overflow-y-auto">
+              {participants.map(
+                (participant, index) =>
+                  // <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
+                  index == 0 ? (
+                    <div className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
+                      <div className="flex items-center justify-center h-8 w-8 bg-orange-200 rounded-full">
+                        {participant.substring(0, 1)}
+                      </div>
+                      <div className="ml-2 text-m font-semibold">
+                        {participant} (방장)
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
+                      <div className="flex items-center justify-center h-8 w-8 bg-pink-200 rounded-full">
+                        {participant.substring(0, 1)}
+                      </div>
+                      <div className="ml-2 text-m font-semibold">
+                        {participant}
+                      </div>
+                    </div>
+                  )
+
+                // </button>
+              )}
             </div>
-            <div className="flex flex-row items-center justify-between text-xs mt-6">
-              <span className="font-bold">Archivied</span>
-              <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
-                7
-              </span>
-            </div>
-            <div className="flex flex-col space-y-1 mt-4 -mx-2">
-              <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
-                <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
-                  H
-                </div>
-                <div className="ml-2 text-sm font-semibold">Henry Boyd</div>
-              </button>
-            </div>
+
+            <button
+              onClick={leaveRoom}
+              className="absolute bottom-5 px-20 py-2 mr-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl text-center items-center"
+            >
+              <span>방 나가기</span>
+            </button>
           </div>
         </div>
         <div className="flex flex-col flex-auto h-full p-6">
@@ -104,156 +157,37 @@ export default function ChatPage() {
             <div className="flex flex-col h-full overflow-x-auto mb-4">
               <div className="flex flex-col h-full">
                 <div className="grid grid-cols-12 gap-y-2">
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                        <div>Hey How are you today?</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                        <div>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Vel ipsa commodi illum saepe numquam maxime
-                          asperiores voluptate sit, minima perspiciatis.
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div className="flex items-center justify-start flex-row-reverse">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                        <div>I'm ok what about you?</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div className="flex items-center justify-start flex-row-reverse">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                        <div>
-                          Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                        <div>Lorem ipsum dolor sit amet !</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div className="flex items-center justify-start flex-row-reverse">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                        <div>
-                          Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                        </div>
-                        <div className="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
-                          Seen
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                        <div>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Perspiciatis, in.
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {/* <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                        <div className="flex flex-row items-center">
-                          <button className="flex items-center justify-center bg-indigo-600 hover:bg-indigo-800 rounded-full h-8 w-10">
-                            <svg
-                              className="w-6 h-6 text-white"
-                              fill="none"
-                              stroke="currentColor"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="1.5"
-                                d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                              ></path>
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="1.5"
-                                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                              ></path>
-                            </svg>
-                          </button>
-                          <div className="flex flex-row items-center space-x-px ml-4">
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-12 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-6 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-5 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-3 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-1 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-1 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                            <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
+                  {messages.map((message, index) =>
+                    message.sender == true ? (
+                      <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                        <div className="flex items-center justify-start flex-row-reverse">
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-blue-400 flex-shrink-0">
+                            {message.username.substring(0, 1)}
+                          </div>
+                          <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                            <div>{message.content}</div>
+                            <div className="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
+                              {message.createdAt}
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div> */}
+                    ) : (
+                      <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                        <div className="flex flex-row items-center">
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-400 flex-shrink-0">
+                            {message.username.substring(0, 1)}
+                          </div>
+                          <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                            <div>{message.content}</div>
+                            <div className="absolute text-xs bottom-0 left-0 -mb-5 mr-2 text-gray-500">
+                              {message.createdAt}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  )}
                 </div>
               </div>
             </div>
