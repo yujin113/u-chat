5fa65322800d80cf8a9d312cc23ebcabee5f1c39
yujin113 <claire9585@sookmyung.ac.kr>
2022-11-08 09:36:34 +0900

    [feat] 로그인 엔터 키 반응 추가

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -54,10 +54,18 @@ function LoginPage() {
   const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
+  const onClickEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") {
+      login();
+    }
+  };
 
   return (
     <>
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div
+        className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+        onKeyDown={onClickEnter}
+      >
         <div className="w-full max-w-md space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
