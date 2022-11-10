5f850271104b237d2d8c34db3956663d91f89938
yujin113 <claire9585@sookmyung.ac.kr>
2022-11-10 17:55:49 +0900

    [fix] Room 도메인의 세션 리스트 제거

--- src/main/java/websocket/chat/domain/Room.java
+++ src/main/java/websocket/chat/domain/Room.java
@@ -3,14 +3,11 @@ package websocket.chat.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -27,9 +24,6 @@ public class Room {
 
     @CreatedDate
     private LocalDateTime createdAt;
-
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
-    private List<Session> sessions = new ArrayList<>();
 
     public static Room createRoom(String name) {
         Room room = new Room();
